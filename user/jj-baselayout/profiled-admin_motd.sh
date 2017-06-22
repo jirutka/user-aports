c427727eae6d7ff9dcd357aa520d8f43f77abbfb
Jakub Jirutka <jakub@jirutka.cz>
2017-06-22 17:31:06 +0200

    user/jj-baselayout: new aport

---
+++ user/jj-baselayout/profiled-admin_motd.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# If not running interactively, do nothing.
+[ -n "$PS1" ] || return
+
+# If not connected via SSH, do nothing.
+[ -n "$SSH_CONNECTION" ] || return
+
+# If root, do nothing.
+[ "$USER" != root ] || return
+
+# If $USER is not member of wheel, do nothing.
+id -nG | grep -qw wheel || return
+
+printf '\033[37;1mLast changes in /etc:\033[0m\n'
+
+git -C /etc log \
+	--abbrev-commit \
+	--decorate \
+	--date=short \
+	--format=format:'* %ad: %s  %C(dim white)<%an>%C(reset)' \
+	--reverse -3
+
+logged=$(ps -o args \
+	| sed -En 's/^sshd: (\w+) \[priv\].*/\1/p' \
+	| sort | uniq \
+	| grep -xv "$USER" \
+	| xargs)
+
+if [ -n "$logged" ]; then
+	printf "\n\033[37;1mLogged users:\033[0m $logged\n"
+fi
---
+++ user/jj-baselayout/profiled-sudo_git_vars.sh
@@ -0,0 +1,18 @@
+# This script sets environment variables SUDO_GIT_NAME and SUDO_GIT_EMAIL
+# with values from the user's .gitconfig. The variables are used by
+# /usr/local/sbin/git wrapper script to pass user's name and email to git
+# when executed via sudo root (both sudo git and sudo sh).
+
+# If root, do nothing.
+[ "$USER" != root ] || return
+
+# If $USER is not member of group wheel, do nothing.
+id -nG | grep -qw wheel || return
+
+SUDO_GIT_NAME=$(GIT_CONFIG="$HOME/.gitconfig" \
+	/usr/bin/git config user.name 2>/dev/null || echo "$USER")
+SUDO_GIT_EMAIL=$(GIT_CONFIG="$HOME/.gitconfig" \
+	/usr/bin/git config user.email 2>/dev/null || echo "$USER@localhost")
+
+export SUDO_GIT_NAME
+export SUDO_GIT_EMAIL
